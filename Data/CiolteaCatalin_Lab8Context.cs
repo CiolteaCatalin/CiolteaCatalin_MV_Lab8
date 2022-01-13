34523c93e816b2b9c8f5fd74db2a025bb256fb87
Catalin Danut Cioltea <Cioltea.Sa.Catalin@student.utcluj.ro>
2022-01-13 17:53:49 +0200

    Update

---
+++ Data/CiolteaCatalin_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CiolteaCatalin_Lab8.Models;
+
+namespace CiolteaCatalin_Lab8.Data
+{
+    public class CiolteaCatalin_Lab8Context : DbContext
+    {
+        public CiolteaCatalin_Lab8Context (DbContextOptions<CiolteaCatalin_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CiolteaCatalin_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<CiolteaCatalin_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<CiolteaCatalin_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CiolteaCatalin_Lab8.Data;
+using CiolteaCatalin_Lab8.Models;
+
+namespace CiolteaCatalin_Lab8.Pages.Categories
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly CiolteaCatalin_Lab8.Data.CiolteaCatalin_Lab8Context _context;
+
+        public DetailsModel(CiolteaCatalin_Lab8.Data.CiolteaCatalin_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Category.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
